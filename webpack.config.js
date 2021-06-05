75ef7a57ee6f03082ff0bb7de8a4c664897d18b0
Touch <s.chantouch@mango-byte.com>
2021-06-05 09:09:56 +0700

    build: webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
 const { VueLoaderPlugin } = require("vue-loader");
-
-module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: ["./src/vue-waiting.js"],
-  output: {
-    library: "VueWaiting",
-    libraryTarget: "umd",
-    filename: "vue-waiting.js",
-    globalObject: 'typeof self !== \'undefined\' ? self : this',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader"
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+module.exports = (env, argv) => {
+  return {
+    mode: env.WEBPACK_SERVE ? 'development' : 'production',
+    entry: ["./src/vue-waiting.js"],
+    output: {
+      library: "VueWaiting",
+      libraryTarget: "umd",
+      filename: "vue-waiting.js",
+      globalObject: 'typeof self !== \'undefined\' ? self : this',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader"
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
         }
-      }
-    ]
-  },
-  plugins: [new VueLoaderPlugin()]
+      ]
+    },
+    plugins: [new VueLoaderPlugin()]
+  };
 };
