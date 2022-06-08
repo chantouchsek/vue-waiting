0ef121cfb09e9c762a2deb78b97dd70a33fda15d
Touch <chantouchsek.cs83@gmail.com>
2022-06-08 11:42:51 +0700

    chore: change const position

--- nuxt/vue-waiting.template.js
+++ nuxt/vue-waiting.template.js
@@ -1,11 +1,10 @@
 import Vue from 'vue';
 import VueWaiting from 'vue-waiting';
 
-const pluginOptions = <%= serialize(options) %>
-
 Vue.use(VueWaiting);
 
 export default ({ app }, inject) => {
+    const pluginOptions = <%= serialize(options) %>
     app.waiting = new VueWaiting(pluginOptions)
     inject('wait', app.waiting)
 }
